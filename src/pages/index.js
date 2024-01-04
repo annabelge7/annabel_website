6609001980784cb42f29f3655152a8afd1258fd3
annabelge7 <101362416+annabelge7@users.noreply.github.com>
2024-01-04 11:53:24 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ const HomeContainer = styled.div`
   background-color: #131d37;
   opacity: 1;
   position: relative;
-  border: 15px solid #43354a;
+  border: 20px solid #584660;
 
 /*animation: borderAnimation 5s infinite;  Apply animation to the border */
 
